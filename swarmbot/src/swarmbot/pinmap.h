cc5ad337e770c2ea1fb46942ed5b5660742a9116
NNaka <norihito.naka@tufts.edu>
2015-04-16 11:13:04 -0400

    Working motor and collision detection integration.  TODO: Forward until red.

---
+++ swarmbot/src/swarmbot/pinmap.h
@@ -0,0 +1,51 @@
+// pinmap.h
+// Contains macro definitions to map pins for the final software design
+
+#ifndef PINMAP_H
+#define PINMAP_H
+
+#include "Arduino.h"
+
+// Interrupt pins
+
+#define COLLISION_INT    0 // pin 2
+#define COMM_INT         1 // pin 3
+
+// Overall pins TODO new name
+
+#define GO_SWITCH        14
+#define BOT_SWITCH       15
+#define RED_LED          35
+#define YELLOW_LED       37
+#define GREEN_LED        39
+#define BLUE_LED         41
+
+// Photosensor pins
+
+#define PHOTO_RED_LEFT   10
+#define PHOTO_RED_RIGHT  11
+#define PHOTO_BLUE_LEFT  12
+#define PHOTO_BLUE_RIGHT 13
+
+#define PHOTOLEFT        A0
+#define PHOTORIGHT       A1
+
+// Motor Control pins
+
+#define ML1              24
+#define ML2              25
+#define MR1              30
+#define MR2              31
+
+#define E1               4
+#define E2               5
+
+// Collision Detection pins
+
+#define FL               43
+#define FR               44
+#define L                45
+#define R                46
+#define B                47
+
+#endif // PINMAP_H
---
+++ swarmbot/src/swarmbot/swarmbot.ino
@@ -0,0 +1,190 @@
+#include "pinmap.h"
+#include "photo_sensor.h"
+#include "motor_control.h"
+
+// ----------------------------------------------------------------------------
+// State variables
+static bool isBot1  = true;
+static bool hitWall = false;
+// ----------------------------------------------------------------------------
+
+void bot1(void);
+void bot2(void);
+
+// ----------------------------------------------------------------------------
+// collision.h
+#define NUM_BUMPERS 5
+#define DEBOUNCE_TIME 10000 // ms
+
+// indices into boolean array for each bumper
+// and bumper array for each pin
+#define FRONT_LEFT  0
+#define FRONT_RIGHT 1
+#define LEFT        2
+#define RIGHT       3
+#define BACK        4
+
+// input pins for reading if a bumper is pressed
+
+bool bumperHit[NUM_BUMPERS];
+volatile bool collisionHappened = false;
+const int bumpers[] = { FL, FR, L, R, B };
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// collision.cpp
+void detectCollision(void) {
+    static unsigned long lastInterruptTime;
+    unsigned long currTime = micros();
+
+    if (currTime - lastInterruptTime > DEBOUNCE_TIME) {
+        collisionHappened = true;
+        lastInterruptTime = currTime;
+    }
+}
+// ----------------------------------------------------------------------------
+
+void setup() {
+    // ------------------------------------------------------------------------
+    // Set up general Bot pins
+    //pinMode( GO_SWITCH,  INPUT);
+    //pinMode( BOT_SWITCH, INPUT);
+    // ------------------------------------------------------------------------
+
+    Serial.begin(9600);
+
+    setupPhotosensor();
+    setupMotorControl();
+
+    attachInterrupt(COLLISION_INT, detectCollision, CHANGE);
+    for (int i = 0; i < NUM_BUMPERS; ++i) pinMode(bumpers[i], INPUT);
+}
+
+void loop() {
+    // TEST COLLISION
+    //if (collisionHappened) {
+    //    for (int i = 0; i < NUM_BUMPERS; ++i) {
+    //        bumperHit[i] = digitalRead(bumpers[i]);
+    //
+    //        if (bumperHit[i]) {
+    //            Serial.println(i);
+    //            bumperHit[i] = false;
+    //        }
+    //    }
+    //}
+
+    // TEST MOTOR CONTROL
+    //forward();
+    //delay(1000);
+    //backward();
+    //delay(2000);
+    //stop();
+    //delay(500);
+
+
+
+    //while (!digitalRead(GO_SWITCH)) { delayMicroseconds(1); } // ON
+
+    isBot1 = true; //digitalRead(BOT_SWITCH);
+
+    // GO state
+    if (isBot1) bot1();
+    else        bot2();
+}
+
+void bot1(void) {
+    // Puts bot in motion (`forward()`)
+    forward();
+
+    // Loops until collision (boolean is set in ISR)
+    while (!hitWall) {
+        for (int i = 0; i < NUM_BUMPERS; ++i) {
+            if (digitalRead(bumpers[i])) {
+                hitWall = true;
+            }
+        }
+        delay(1);
+    }
+
+
+    // Assumes it hit the correct wall and turns a predetermined angle to the left
+
+    stop(); delay(1000); // resolve collision by backing up
+    forward();
+    turn(-20); // negative to turn left
+
+    // Moves forward 
+    forward();
+
+    // Stops on red, flashing a red LED
+    int sensorL;
+    int sensorR;
+    do {
+      int r, b;
+      readRightSensor(r, b);
+      sensorL = getColor(r, b);
+      readLeftSensor(r, b);
+      sensorR = getColor(r, b);
+      forward();      
+    } while (sensorL != RED && sensorR != RED);
+    stop();
+    flashLED(RED_LED);
+    
+//    // Stops on red, flashing a red LED
+//    // Turns a predetermined angle to the right, and follows red
+//    // Stops on yellow, flashing yellow LED twice
+//    // Moves forward
+//    // Follows red
+//    // Stops on yellow, turns on yellow LED, turns 180 degrees
+//    // Moves forward until red
+//    // Follows red
+//    // Stops on yellow
+//    // Communicates to Bot 2: `START`
+//    // Waits for `ACK_START`
+//    // Flashes green LED
+//    // Moves forward until red
+//    // Follows red
+//    // TBD // we want this to be close to a specific location
+//    // Turns on green LED
+//    // Waits for `TOXIC`
+//    // Flash yellow LED continuously
+//    // Waits for `STOP_YELLOW`
+//    // Turns off yellow LED
+//    // Waits for `DONE`
+//    // Flashes green LED
+}
+
+void flashLED(int ledPin) {
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+}
+
+void bot2(void) {
+//    // Waits for `START`
+//    // Flash green LED
+//    // Puts bot in motion (`forward()`)
+//    // Loops until collision (boolean is set in ISR)
+//    // Assumes it hit the correct wall and turns a predetermined angle to the left
+//    // Moves forward 
+//    // Stops on blue, flashing a blue LED
+//    // Turns a predetermined angle to the right, and follows blue
+//    // Stops on yellow, flashing yellow LED twice
+//    // Moves forward
+//    // Follows blue
+//    // Stops on yellow, turns on yellow LED, turns 180 degrees
+//    // Communicates to Bot 1: `TOXIC`
+//    // Flash yellow continuously
+//    // Moves forward until blue
+//    // Follows blue
+//    // Stops on yellow
+//    // Communicates to Bot 1: `STOP_YELLOW`
+//    // Stop flashing yellow LED
+//    // Moves forward until blue
+//    // Follows blue
+//    // TBD // we want this to be close to a specific location
+//    // Communicates to Bot 1: `DONE`
+//    // Flashes green LED
+}
+
