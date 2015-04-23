04987b3bef44732d0e73a6192e4cea52ea9b548a
NNaka <norihito.naka@tufts.edu>
2015-04-23 10:45:46 -0400

    Added code for Wifi Module Go-Beyond.

---
+++ wifi/connect_wifi/connect_wifi.ino
@@ -0,0 +1,15 @@
+#include <SPI.h>
+#include <WiFi.h>
+#include "wifi_control.h"
+
+void setup() {
+  Serial.begin(9600);
+  connectWifi();
+  printWifiStatus();
+}
+
+void loop() {
+  postData("Good", true, true);
+}
+
+
